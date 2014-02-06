cb9532ec910190d3c184e1520f53545ed26cc98f
Sam Rijs <srijs@users.noreply.github.com>
2014-02-06 03:10:46 +0100

    run dxr install as root

--- install.sh
+++ install.sh
@@ -12,5 +12,5 @@ export LLVM_CONFIG=$PWD/clang+llvm-3.2-x86_64-linux-ubuntu-12.04/bin/llvm-config
 cd dxr
 sudo pip install -r requirements.txt
 make
-python setup.py install
+sudo python setup.py install
 cd tests/test_basic && make
