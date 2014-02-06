33bf07197b8224dfb155b543013df5c582a3de1a
Sam Rijs <srijs@users.noreply.github.com>
2014-02-06 16:45:15 +0100

    try to deploy dxr from submodule

--- server.sh
+++ server.sh
@@ -2,5 +2,9 @@
 
 export DXR_FOLDER=$PWD/target
 export LD_LIBRARY_PATH=$PWD/dxr/trilite
-cd dxr/dxr
+export PYTHONPATH=$PWD/dxr:$PWD/dxr/trilite:$PYTHONPATH
+cp libtrilite.so dxr/trilite
+cd dxr
+python setup.py install
+cd dxr
 gunicorn wsgi:application
